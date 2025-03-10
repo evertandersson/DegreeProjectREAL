a6642e3f67cd1b6a53d5e7d5781799517fb59203
evertandersson <evert.andersson@edu.futuregames.nu>
2025-03-10 11:20:40 +0100

    First commit

---
+++ Source/DegreeProject/DegreeProject.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DegreeProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DegreeProject, "DegreeProject" );
+ 
