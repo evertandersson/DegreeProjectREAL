a1794fea8a2b59180d7818764b3919a780a4d982
Liamyaboi <thebest03liam@gmail.com>
2025-03-11 14:36:57 +0100

    ????

--- Source/DegreeProject/DegreeProject.Build.cs
+++ Source/DegreeProject/DegreeProject.Build.cs
@@ -1,6 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
+using UnrealBuildTool.Rules;
 
 public class DegreeProject : ModuleRules
 {
@@ -8,6 +9,6 @@ public class DegreeProject : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "AIModule", "GameplayTasks", "NavigationSystem"  });
 	}
 }
