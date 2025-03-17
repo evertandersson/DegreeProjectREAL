b8cd28256688886320f1eca2f8a0f610f8f16516
evertandersson <evert.andersson@edu.futuregames.nu>
2025-03-17 13:45:50 +0100

    fixed a bug that caused the grapple hook to not disable when dashing

--- Source/DegreeProject/DegreeProjectCharacter.h
+++ Source/DegreeProject/DegreeProjectCharacter.h
@@ -169,6 +169,9 @@ public:
 	UPROPERTY(EditAnywhere, Category = Player)
 	float DashDuration;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Player)
+	bool bIsDashing = false;
+
 private:
 	// Function to handle attribute changes
 
@@ -177,8 +180,7 @@ private:
 
 	UPROPERTY(EditAnywhere, Category = Player)
 	bool bCanDash;
-	UPROPERTY(EditAnywhere, Category = Player)
-	bool bIsDashing = false;
+
 	// Specifies which properties should be replicated over the network
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
