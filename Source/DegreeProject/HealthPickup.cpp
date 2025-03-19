ed1daabb8a0b7d9ef1ff4b24202db6e9e7cb0718
RipKowai <georgi.damyanov@edu.futuregames.nu>
2025-03-19 16:40:50 +0100

    sd

---
+++ Source/DegreeProject/HealthPickup.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HealthPickup.h"
+
+// Sets default values
+AHealthPickup::AHealthPickup()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AHealthPickup::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AHealthPickup::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AHealthPickup::OnPickup(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* otherComp,
+	int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResults)
+{
+	if (OtherActor->ActorHasTag("Player"))
+	{
+		Destroy();
+	}
+}
