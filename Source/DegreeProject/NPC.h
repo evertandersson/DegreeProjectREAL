883297b0abf432a14385292234a5739929143969
Liamyaboi <thebest03liam@gmail.com>
2025-03-19 16:04:29 +0100

    AI will chase you and have created BP for patrol points

--- Source/DegreeProject/NPC.h
+++ Source/DegreeProject/NPC.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "PatrolPath.h"
 #include "BehaviorTree/BehaviorTree.h"
 #include "GameFramework/Character.h"
 #include "NPC.generated.h"
@@ -28,8 +29,10 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
+private:
 	UPROPERTY(EditAnyWhere, BlueprintReadWrite, Category = "AI", meta=(AllowPrivateAccess = "true"))
 	UBehaviorTree* Tree;
 
+	UPROPERTY(EditAnyWhere, BlueprintReadWrite, Category = "AI", meta = (AllowPrivateAccess = "true"))
+	APatrolPath* PatrolPath;
 };
