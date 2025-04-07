c17fb8eeaac5af8ebd483035f5280de79d7c125b
evertandersson <evert.andersson@edu.futuregames.nu>
2025-04-07 12:13:41 +0200

    more cleanup and bug fixes

--- Source/DegreeProject/Private/MantleComponent.cpp
+++ Source/DegreeProject/Private/MantleComponent.cpp
@@ -11,103 +11,85 @@
 UMantleComponent::UMantleComponent()
 {
 	PrimaryComponentTick.bCanEverTick = false;
+
+    bIsClimbing = false;
 }
 
 float UMantleComponent::MantleTrace(ADegreeProjectCharacter* Player, float Length)
 {
-    FVector PlayerPos = Player->GetActorLocation();
-    PlayerPos.Z += 150.f;
-    FVector PlayerForward = Player->GetActorForwardVector() * Length;
+    if (!Player) return 0.0f;
 
-    FVector StartLocation = PlayerPos + PlayerForward;
-    FVector EndLocation = FVector(StartLocation.X, StartLocation.Y, StartLocation.Z - 200.f);
+    FVector PlayerPos = Player->GetActorLocation() + FVector(0.f, 0.f, 150.f);
+    FVector ForwardOffset = Player->GetActorForwardVector() * Length;
 
-    float Radius = 10.f;
+    FVector StartLocation = PlayerPos + ForwardOffset;
+    FVector EndLocation = StartLocation - FVector(0.f, 0.f, 200.f);
 
-    TArray<AActor*> ActorsToIgnore;
-    ActorsToIgnore.Add(Player);
+    constexpr float Radius = 10.f;
+    constexpr float MaxMantleAngle = 40.f;
 
-    TArray<TEnumAsByte<EObjectTypeQuery>> ObjectTypesArray;
-    ObjectTypesArray.Add(UEngineTypes::ConvertToObjectType(ECollisionChannel::ECC_WorldStatic));
+    TArray<AActor*> ActorsToIgnore = { Player };
+    TArray<TEnumAsByte<EObjectTypeQuery>> ObjectTypesArray = {
+        UEngineTypes::ConvertToObjectType(ECollisionChannel::ECC_WorldStatic)
+    };
 
     FHitResult HitResult;
-
-    bool bHit = UKismetSystemLibrary::SphereTraceSingleForObjects(
-        this,
-        StartLocation,
-        EndLocation,
-        Radius,
-        ObjectTypesArray,
-        false,
-        ActorsToIgnore,
-        EDrawDebugTrace::None,
-        HitResult,
-        true
-    );
-
-    if (HitResult.bBlockingHit)
-    {
-        MantleHeight = HitResult.Location;
-
-        FVector StartLocation2 = Player->GetActorLocation();
-        StartLocation2.Z += 130.f;
-        
-        FVector PlayerForward2 = Player->GetActorForwardVector() * 100.f;
-        FVector EndLocation2 = StartLocation2 + PlayerForward2;
-
-        FHitResult HitResult2;
-
-        bool bHit2 = UKismetSystemLibrary::SphereTraceSingleForObjects(
-            this,
-            StartLocation2,
-            EndLocation2,
-            20.f,
-            ObjectTypesArray,
-            false,
-            ActorsToIgnore,
-            EDrawDebugTrace::None,
-            HitResult2,
-            true
-        );
-
-        if (HitResult2.bBlockingHit)
-        {
-            MantleType = Enum_MantleType::NoMantle;
-            return 0.0f;
-        }
-        else 
-        {
-            UMeshComponent* MeshComponent = Player->GetMesh();
-            FVector HeadLocation = MeshComponent->GetSocketLocation("head");
-            FVector CalfLocation = MeshComponent->GetSocketLocation("calf_l");
-
-            if (MantleHeight.Z > HeadLocation.Z
-                && UKismetMathLibrary::DegAcos(UKismetMathLibrary::Dot_VectorVector(HitResult.ImpactNormal, FVector(0, 0, 1.f))) < 40.f
-                && HitResult.Distance > 0.0f)
-            {
-                MantleType = Enum_MantleType::Mantle2M;
-                return MantleHeight.Z - 120.f;
-            }
-            else if (MantleHeight.Z > CalfLocation.Z
-                && UKismetMathLibrary::DegAcos(UKismetMathLibrary::Dot_VectorVector(HitResult.ImpactNormal, FVector(0, 0, 1.f))) < 40.f
-                && HitResult.Distance > 0.0f)
-            {
-                MantleType = Enum_MantleType::Mantle1M;
-                return MantleHeight.Z - 30.f;
-            }
-            else 
-            {
-                MantleType = Enum_MantleType::NoMantle;
-                return 0.0f;
-            }
-        }
-    }
-    else 
+    if (!UKismetSystemLibrary::SphereTraceSingleForObjects(
+        this, StartLocation, EndLocation, Radius, ObjectTypesArray, false,
+        ActorsToIgnore, EDrawDebugTrace::None, HitResult, true))
     {
         MantleType = Enum_MantleType::NoMantle;
         return 0.0f;
     }
+
+    MantleHeight = HitResult.Location;
+
+    // Check for obstacles in front
+    if (IsObstacleInFront(Player))
+    {
+        MantleType = Enum_MantleType::NoMantle;
+        return 0.0f;
+    }
+
+    return DetermineMantleHeight(Player, HitResult, MaxMantleAngle);
 }
 
+bool UMantleComponent::IsObstacleInFront(ADegreeProjectCharacter* Player)
+{
+    FVector StartLocation = Player->GetActorLocation() + FVector(0.f, 0.f, 130.f);
+    FVector EndLocation = StartLocation + (Player->GetActorForwardVector() * 100.f);
+
+    FHitResult HitResult;
+    return UKismetSystemLibrary::SphereTraceSingleForObjects(
+        this, StartLocation, EndLocation, 20.f,
+        { UEngineTypes::ConvertToObjectType(ECollisionChannel::ECC_WorldStatic) },
+        false, { Player }, EDrawDebugTrace::None, HitResult, true);
+}
+
+float UMantleComponent::DetermineMantleHeight(ADegreeProjectCharacter* Player, const FHitResult& HitResult, float MaxAngle)
+{
+    UMeshComponent* MeshComponent = Player->GetMesh();
+    FVector HeadLocation = MeshComponent->GetSocketLocation("head");
+    FVector CalfLocation = MeshComponent->GetSocketLocation("calf_l");
+
+    float ImpactAngle = UKismetMathLibrary::DegAcos(UKismetMathLibrary::Dot_VectorVector(HitResult.ImpactNormal, FVector(0, 0, 1.f)));
+
+    if (ImpactAngle < MaxAngle && HitResult.Distance > 0.0f)
+    {
+        if (MantleHeight.Z > HeadLocation.Z)
+        {
+            MantleType = Enum_MantleType::Mantle2M;
+            return MantleHeight.Z - 120.f;
+        }
+        if (MantleHeight.Z > CalfLocation.Z)
+        {
+            MantleType = Enum_MantleType::Mantle1M;
+            return MantleHeight.Z - 30.f;
+        }
+    }
+
+    MantleType = Enum_MantleType::NoMantle;
+    return 0.0f;
+}
 
 
